98d0bbd5526e4f3151a9ad8dbeb1cedd58cd2542
Margarita Vankevich <margaritavankevich@gmail.com>
2024-03-07 22:38:49 +0300

    main

--- ReadingClubAPI_.Net/Models/DAL.cs
+++ ReadingClubAPI_.Net/Models/DAL.cs
@@ -8,13 +8,13 @@ namespace Log_inWeb_Api.Models
     public class DAL
     {
         private static int webapicon;
-        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings[webapicon].ConnectionString);
-        SqlDataAdapter da = null;
+        //SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings[webapicon].ConnectionString);
+        //SqlDataAdapter da = null;
         DataTable dt = null;
 
         public string GetLogin(LoginModel loginModel)
         {
-            da = new SqlDataAdapter("Select * from Users where UserName = '"+ loginModel.Username+"' and Password = '"+ loginModel.Password +"' ", con);
+            //da = new SqlDataAdapter("Select * from Users where UserName = '"+ loginModel.Username+"' and Password = '"+ loginModel.Password +"' ", con);
             dt = new DataTable();
             da.Fill(dt);    
             if(dt.Rows.Count>0)
