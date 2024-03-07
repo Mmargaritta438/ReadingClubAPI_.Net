9dc017c41f9095acb48bbc382f47a159d992b4e0
Margarita Vankevich <margaritavankevich@gmail.com>
2024-03-07 21:13:22 +0300

    main

---
+++ BookWebAppAutorization/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BookWebAppAutorization.Data.Indentity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookWebAppAutorization.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationIdentityUser, IdentityRole, string>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ BookWebAppAutorization/Data/Indentity/ApplicationIdentityUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BookWebAppAutorization.Data.Indentity
+{
+    public class ApplicationIdentityUser : IdentityUser
+    {
+        public long AppllicationId { get; set; }
+    }
+}
